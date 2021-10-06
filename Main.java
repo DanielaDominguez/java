afb8b8dfb60f2f6f553d6f15e6fa177b098f9888
Daniela Dominguez Peña <daniela.dominguezz.p@gmail.com>
2021-10-06 00:18:02 -0500

    mi primer hola mundo desde java

---
+++ Main.java
@@ -0,0 +1,7 @@
+//main es la clase principal
+//siempre se debe usar comillas dobles y nunca olvidar ;
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Hola mundo desde Java.");
+    }
+}
